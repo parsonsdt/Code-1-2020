52703f1b847dad59a481a130d008b79105212a3e
bennorskov <norskovb@newschool.edu>
2020-08-30 21:25:06 -0400

    updated class assignments page and put in conditional code for the sub-nav

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let main;
-function activateSubNav() {
+function activateSubNav(options) {
   main = document.getElementById("main-panel");
 
   // I believe the code below should work for any sub-nav items we may want to put in there
@@ -11,8 +11,15 @@ function activateSubNav() {
       this.classList.remove("sub-nav__item");
       // if we remove that class name, then the only one left is the key for the dataStruct
       // The event listener will still exist on the DOMobject even without the classname that added it
-      // console.log(dataStruct[this.classList[0]] );
-      main.innerHTML = dataStruct[this.classList[0]];
+      // console.log(dataStruct[this.classList[0]]);
+      let _html = dataStruct[this.classList[0]];
+
+      // we can add in conditional page footer/headers here based on the passed through argument. Without one, it's ignored. 
+      if (options === "assignments") {
+        _html += "<hr><h1>How to turn in your work</h1><p>Upload to the class github</p>";
+      }
+
+      main.innerHTML = _html;
     });
   }
 }
