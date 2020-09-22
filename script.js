a5e770ee725680e205dd3391832b56518f9d0090
bennorskov <norskovb@newschool.edu>
2020-09-22 06:00:16 -0400

    updated the options handler

--- script.js
+++ script.js
@@ -16,10 +16,7 @@ function activateSubNav(options) {
       // console.log(dataStruct[this.classList[0]]);
       let _html = dataStruct[this.classList[0]];
 
-      // we can add in conditional page footer/headers here based on the passed through argument. Without one, it's ignored. 
-      if (options === "assignments") {
-        _html += "<hr><h1>How to turn in your work</h1><ol><li>Save your sketch on <a href='https://editor.p5js.org/'>p5js's web editor</a>.</li><li>Title your sketch in this format:<br>&ldquo;Code 1, Assignment 1: Instructional Drawing&rdquo; or &ldquo;Code 1, Assignment 3: Personal Time&rdquo;</li><li>In a comment at the top of your sketch include:<br><ul><li>Your name</li><li>Your faculty (Xin, Courtney, or Ben)</li></ul></li><li>Place a link to your sketch on Canvas</li></ol>";
-      }
+      _html += handleOptions(options);
 
       main.innerHTML = _html;
     });
@@ -31,6 +28,14 @@ function checkForURLDirectLink() {
   console.log(dataStruct[autoPopulate], autoPopulate);
 
   if (autoPopulate != undefined && dataStruct[autoPopulate] != undefined) {
-    main.innerHTML = dataStruct[autoPopulate];
+    main.innerHTML = dataStruct[autoPopulate] + handleOptions(options);
   }
+}
+
+function handleOptions(options) {
+  let toReturn = '';
+  if (options === "assignments") {
+    toReturn += "<hr><h1>How to turn in your work</h1><ol><li>Save your sketch on <a href='https://editor.p5js.org/'>p5js's web editor</a>.</li><li>Title your sketch in this format:<br>&ldquo;Code 1, Assignment 1: Instructional Drawing&rdquo; or &ldquo;Code 1, Assignment 3: Personal Time&rdquo;</li><li>In a comment at the top of your sketch include:<br><ul><li>Your name</li><li>Your faculty (Xin, Courtney, or Ben)</li></ul></li><li>Place a link to your sketch on Canvas</li></ol>";
+  }
+  return toReturn;
 }
